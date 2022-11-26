0a9a7fabf6c5a8b511ab0f7059bd08a2a311bc49
czekanskyy <czekanski.dominik@proton.me>
2022-11-26 15:54:11 +0100

    Created /dashboard main page

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import PageStats from './components/main/PageStats';
 import PopularPostsView from './components/main/PopularPostsView';
 import QuickActions from './components/main/QuickActions';
@@ -6,14 +5,15 @@ import RecentCommentsView from './components/main/RecentCommentsView';
 
 const DashboardPage = async () => {
   return (
-    <>
-      <h1 className='bold text-2xl'>Dashboard</h1>
-      <Link href='/' className='bg-blue-500 text-white p-2'>
-        Back to home
-      </Link>
-      <h2>test</h2>
-      <h2>test2</h2>
-    </>
+    <div className='flex flex-col gap-y-8'>
+      <QuickActions />
+      {/* @ts-ignore */}
+      <PageStats />
+      {/* @ts-ignore */}
+      <PopularPostsView />
+      {/* @ts-ignore */}
+      <RecentCommentsView />
+    </div>
   );
 };
 
